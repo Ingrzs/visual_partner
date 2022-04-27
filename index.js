452a864732d1ee0d3f0d179372b3782269f0c38c
Ingrzs <ing.ramirozs@gmail.com>
2022-04-27 11:49:25 -0500

    debug index de app de Reader

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const Reader = require("./lib/utils/Reader");
+const Reader = require("./lib/utils/Reader"); 
+//const ExplorerService = require ("./lib/services/ExplorerService")
+
 const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
-console.log(explorers)
+//console.log(explorers)
+
+const explorersbyMission = explorers.filter((explorer) => explorer.mission == "node");
+//console.log(explorersbyMission.length)
+
+
+const usernamesInNode = explorersbyMission.map((explorer) => explorer.Username == "node");
+//console.log(usernamesInNode.length)
+
+
+const assignFizzBuzzTrick = function(explorer){
+    if(explorer.score%5 === 0 && explorer.score%3 === 0){
+        explorer.trick = "FIZZBUZZ";
+        return explorer;
+    }else{
+        explorer.trick = explorer.score;
+        return explorer;
+    }
+};
+
+const explorersInNodeAndFizzBuzzTrick = explorersbyMission.map((explorer) => assignFizzBuzzTrick(explorer));
+console.log(explorersInNodeAndFizzBuzzTrick)
