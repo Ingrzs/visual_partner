1338388bfb3ad43dd836b43dba5718ea3c062987
Ingrzs <ing.ramirozs@gmail.com>
2022-04-27 23:52:32 -0500

    pruebas de fizzbuzz actualizadas

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,28 +1,35 @@
 const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
 describe("Pruebas unitarias Fizbuzz",()=>{
+ test ('test para validar 1',()=>{
+    const explorer1 = { name: "Explorer1", score: 1 };
+    const Fizzbuss1 = FizzbuzzService.applyValidationInExplorer(explorer1);
+    expect(Fizzbuss1.trick).toBe(1)
 
-    test("Requerimiento uno explorer con trick 1", () =>{
-        const explorer1 = {name:"Explorer1",score:1 };
-        const explorersbyMission =  FizzbuzzService.applyValidationInExplorer(explorer1);
-        expect(explorersbyMission).toBe(explorer1,1);
-    });
-    
-    test("Requerimiento dos explorer con FIZZ", () =>{
-        const explorer3 = {name:"Explorer3",score:3 };
-        const explorersbyMission =  FizzbuzzService.applyValidationInExplorer(explorer3);
-        expect(explorersbyMission).toBe(explorer3,"FIZZ");
-    });
+ })
 
-    test("Requerimiento tres explorer con BUZZ", () =>{
-        const explorer5 = {name:"Explorer5",score:5 };
-        const explorersbyMission =  FizzbuzzService.applyValidationInExplorer(explorer5);
-        expect(explorersbyMission).toBe(explorer5,"BUZZ");
-    });
+ test ('test para validar FIZZ',()=>{
+    const explorer3 = { name: "Explorer3", score: 3 };
+    const Fizzbuss3 = FizzbuzzService.applyValidationInExplorer(explorer3);
+    expect(Fizzbuss3.trick).toBe("FIZZ")
 
-    test("Requerimiento cuatro explorer con FIZZBUZZ", () =>{
-        const explorer15 = {name:"Explorer15",score:15 };
-        const explorersbyMission =  FizzbuzzService.applyValidationInExplorer(explorer15);
-        expect(explorersbyMission).toBe(explorer15,"FIZZBUZZ");
-    });
-});
+ })
+
+ test ('test para validar BUZZ',()=>{
+    const explorer5 = { name: "Explorer5", score: 5 };
+    const Fizzbuss5 = FizzbuzzService.applyValidationInExplorer(explorer5);
+    expect(Fizzbuss5.trick).toBe("BUZZ")
+
+ })
+
+ test ('test para validar FIZZBUZZ',()=>{
+    const explorer15 = { name: "Explorer15", score: 15 };
+    const Fizzbuss15 = FizzbuzzService.applyValidationInExplorer(explorer15);
+    expect(Fizzbuss15.trick).toBe("FIZZBUZZ")
+
+ })
+
+
+
+
+})
