45d361c75d45e8eda7e98c35982f8dd06b4439f0
Ingrzs <ing.ramirozs@gmail.com>
2022-04-28 00:49:09 -0500

    agregando test para controller

---
+++ test/services/ExplorerControlller.test.js
@@ -0,0 +1,13 @@
+const { getExplorersByMission } = require("./../../lib/controller/ExplorerController");
+const ExplorerController = require("./../../lib/controller/ExplorerController");
+
+describe("Pruebas unitarias Explorer Controller",()=>{
+    test ("test para validar 1",()=>{
+        const explorerbymission = ExplorerController.getExplorersByMission("node");
+        expect(explorerbymission[0].name).toBe("Woopa1");
+
+    });
+   
+    
+
+})
