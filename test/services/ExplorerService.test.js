cdf6074934777afa980bdd3411a312ff29a5a5d8
Ingrzs <ing.ramirozs@gmail.com>
2022-04-27 23:41:23 -0500

    test nuevo para ExplorerServices.js

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,26 +1,36 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
 
-describe("Pruebas unitarias para services",()=>{
-
-    test("Requerimiento uno explorer en Node", () =>{
-        const explorers = [{mission:"node"}];
-        const explorersbyMission =  ExplorerService.filterByMission( explorers ,"node");
-        expect(explorersbyMission.length).toBe(1);
-    });
 
 
-
-    test("Requerimiento tres explorer en Node", () =>{
-        const explorers = [{mission:"node"}];
-        const AmountOfExplorers =  ExplorerService.getAmountOfExplorersByMission( explorers ,"node");
-        expect(AmountOfExplorers.length).toBe(1);
-    });
+    describe("ExplorerService test", () => {
+        test("Test ByMission", () => {
+            const explorer = [{ mission: "node" }];
+            const filterExplorer = ExplorerService.filterByMission(explorer, "node");
     
+            expect(filterExplorer.length).toBe(1);
+        });
+    
+        test("test de cantidad ByMission", () => {
+            const explorer = [{ mission: "node" }, { mission: "node" }];
+            const filterExplorer = ExplorerService.getAmountOfExplorersByMission(
+                explorer,
+                "node"
+            );
+    
+            expect(filterExplorer).toBe(2);
+        });
+    
+        test("Test por UsernamesByMission", () => {
+            const explorer = [
+                { mission: "node", githubUsername: "Ramiro" },
+                { mission: "node", githubUsername: "rzs" },
+            ];
+            const filterExplorer = ExplorerService.getExplorersUsernamesByMission(
+                explorer,
+                "node"
+            );
+    
+            expect(filterExplorer[0]).toBe("Ramiro");
+        });
+})
 
-    test("Requerimiento tres explorer en Node", () =>{
-        const explorers = [{mission:"node"}];
-        const ExplorersUsernamesByMission =  ExplorerService.getExplorersUsernamesByMission( explorers ,"node");
-        expect(ExplorersUsernamesByMission.length).toBe(1);
-    });
-
-});
